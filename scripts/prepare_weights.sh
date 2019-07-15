9b0631b0b74e8e18158f934b15c8d37aa602fca0
Robin van Boven <497556+Beanow@users.noreply.github.com>
2019-07-15 21:27:39 +0200

    Update the sourcecred dependency (#2)
    * Update the sourcecred dependency to base off the timeline release (v0.3.0)
    * Include upstream merged --weights commit.

--- scripts/prepare_weights.sh
+++ scripts/prepare_weights.sh
@@ -2,11 +2,11 @@
 # Make sure the pwd is the root of the repository.
 
 # Rebuild weight overrides from root toml file.
-rm sourcecred/weights.json
+rm .weights.json
 if [ -e "weights.toml" ]; then
 	echo "Converting weights.toml"
 	cd mkweights
 	npm install --production
 	cd ..
-	cat weights.toml | node mkweights > sourcecred/weights.json
+	cat weights.toml | node mkweights > .weights.json
 fi
